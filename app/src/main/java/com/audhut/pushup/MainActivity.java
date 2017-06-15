91ef324b15db14f1d1e0717c972f7dccf0926896
Sunil Bhalerao <sunilbhalerao14@gmail.com>
2017-06-16 00:14:08 +0530

    Modified MainActivity

--- app/src/main/java/com/audhut/pushup/MainActivity.java
+++ app/src/main/java/com/audhut/pushup/MainActivity.java
@@ -2,6 +2,7 @@ package com.audhut.pushup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,10 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testUpload();
     }
 
     private void testUpload(){
 
         int  a = 1;
+
+        a = a + 10;
+
+        Toast.makeText(this," Value of a = " + a,Toast.LENGTH_SHORT).show();
+
+
     }
 }
